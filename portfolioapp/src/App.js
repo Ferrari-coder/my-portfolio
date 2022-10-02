9f59fdf27fb0d090231328448938cb2f28693468
Oluwaferanmi Aiyegbusi <oluwaferanmiaiyegbusi@gmail.com>
2022-10-03 00:33:11 +0100

    portfolio added

--- portfolioapp/src/App.js
+++ portfolioapp/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 // import AboutMe from "./about";
 import Header from "./header";
 import Services from "./services";
+import ShowCase from "./portfolio";
 
 
 
@@ -11,6 +12,7 @@ function App() {
     <body>
       <Header/>
       <Services/>
+      <ShowCase/>
     </body>
   )
 }
